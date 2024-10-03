e21cea1fba234c75f73008a46da5bf6d677f08eb
Munziu <158445162+Munziu@users.noreply.github.com>
2024-10-03 10:20:52 +0300

    Update databaseConnection.php

--- inc/databaseConnection.php
+++ inc/databaseConnection.php
@@ -1,10 +1,15 @@
 <?php
-    $serverName = "localhost";
-    $userName = "root";
-    $password = "";
+    $serverName = "localhost";  // Database server, usually 'localhost'
+    $userName = "root";  // MySQL username, default is 'root'
+    $password = "";  // MySQL password, usually empty for local dev environments
+    $dbName = "login_info";  // The name of your database
 
-    $conn = new mysqli($serverName,$userName,$password);
-    if($conn->connect_error){
-        die("Connection Failed: ".$conn->connect_error);
+    // Create connection
+    $conn = new mysqli($serverName, $userName, $password, $dbName);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
     }
+    echo "Connected successfully!";
 ?>
